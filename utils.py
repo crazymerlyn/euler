0623f9fc2792a27eeb4cadf4628c4525e50cf9e6
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-07-23 09:56:54 +0530

    Add some utility functions

--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
+def primes(n):
+    isprime = [True for _ in range(n+1)]
+    for i in range(2, n+1):
+        if not isprime[i]: continue
+        for j in range(i*i, n+1, i):
+            isprime[j] = False
+
+    res = []
+    for i in range(2, n+1):
+        if isprime[i]: res.append(i)
+
+    return res
+
 def factorize(n):
     res = []
     i = 2
@@ -18,4 +31,7 @@ def phi(n):
     for prime, power in factorize(n):
         res *= prime ** power - prime ** (power-1)
     return res
+
+def is_prime(n):
+    return n > 1 and all(n % i for i in range(2, int(n**0.5)+1))
 
