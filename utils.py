f14ef25f62a322816b8db066ceadbdcfc3f5be15
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-07-26 10:08:16 +0530

    Add some utilities

--- utils.py
+++ utils.py
@@ -44,3 +44,34 @@ def is_prime2(n):
             return False
     return True
 
+wheel = [2,4,2,4,6,2,6,4,2,4,6,6,2,6,4,2,6,4,6,8,4,2,4,2,
+         4,8,6,4,6,2,4,6,2,6,6,4,2,4,6,2,6,4,2,4,2,10,2,10]
+wsize = 48
+
+def primes2():
+    for x in (2, 3, 5, 7): yield x
+    for x in wsieve(): yield x
+
+def wsieve():       # wheel-sieve, by Will Ness. cf. ideone.com/WFv4f
+    yield 11        # cf. https://stackoverflow.com/a/10733621/849891
+    mults = {}      #     https://stackoverflow.com/a/19391111/849891
+    ps = wsieve()
+    p = next(ps)
+    psq, c, i = p*p, 11, 0                   # 13 = 11 + wheel[0]
+    cbase, ibase = 11, 0
+    while True:
+        c += wheel[i] ; i = (i+1) % wsize    # 17 = 13 + wheel[1]
+        if c in mults:
+            (j,pbase) = mults.pop(c)         # add(mults, NEXT c, j, pbase)
+        elif c < psq:
+            yield c ; continue
+        else:          # (c==psq)
+            while not cbase == p:
+                cbase += wheel[ibase]
+                ibase = (ibase+1) % wsize    # ibase - initial offset into wheel, for p
+            j, pbase = ibase, p              # add(mults, NEXT c, ibase, p)
+            p = next(ps) ; psq = p*p
+        m = c + pbase*wheel[j] ; j = (j+1) % wsize    # mults(p) = map (*p)
+        while m in mults:                             #              roll(wheel,ibase,p)
+            m += pbase*wheel[j] ; j = (j+1) % wsize
+        mults[m] = (j,pbase)
