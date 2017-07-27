29ceacc2d073a7a3acd80870c3887a9c2c013e84
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-07-27 09:58:43 +0530

    Use less memory

--- utils.py
+++ utils.py
@@ -33,7 +33,7 @@ def phi(n):
     return res
 
 def is_prime(n):
-    return n > 1 and all(n % i for i in range(2, int(n**0.5)+1))
+    return n > 1 and all(n % i for i in xrange(2, int(n**0.5)+1))
 
 def is_prime2(n):
     import random
