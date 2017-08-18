ebd49af320a028c40867658b04ba2c2b3e4e4e7a
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-08-18 09:24:35 +0530

    Solved 88

---
+++ 88.py
@@ -0,0 +1,13 @@
+def prodsum(p, s, c, start):
+    k = p - s + c
+    if k < kmax:
+        if p < n[k]: n[k] = p
+        for i in xrange(start, kmax//p*2 + 1):
+            prodsum(p*i, s+i, c+1, i)
+
+kmax = 12001
+n = [2*kmax] * kmax
+
+prodsum(1, 1, 1, 2)
+
+print sum(set(n[2:]))
