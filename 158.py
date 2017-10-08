9f1542e368fd3f9041209dbf6557c2b5fa67dba1
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-08 10:21:36 +0530

    Improve solution to Euler 158

--- 158.py
+++ 158.py
@@ -5,20 +5,7 @@ def comb(n, r):
     return factorial(n) / factorial(n - r) / factorial(r)
 
 def p(n):
-    base = comb(26, n)
-    res = 0
-
-    for right in range(2, n+1):
-        for left in range(1, right):
-            middle = right - left - 1
-            greater = n - right
-            for lcount in range(n-right, n - left):
-                for middlecount in range(lcount-greater, min(lcount, middle) + 1):
-                    if (left - 1 + middle - middlecount) < (n - lcount - 2): continue
-                    count = comb(middle, middlecount) * comb(greater, lcount-middlecount)
-                    res += count
-
-    return base * res
+    return comb(26, n) * (2 ** n - n - 1)
 
 print max(p(n) for n in range(1, 27))
 
